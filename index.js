ed9f86db08ce3ac5117c8912666334ae714532d5
Jussi Vatjus-Anttila <jussiva@gmail.com>
2015-09-28 08:07:17 +0300

    styling, policy changes which messages allow to handle

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var winston = require('winston');
 var mongoose = require('mongoose');
 var Slack = require('slack-client');
@@ -5,12 +6,18 @@ var nconf = require('nconf');
 var winston = require('winston');
     
 function AddonSlack (app, server, io, passport){
-  var self = this;
+    var self = this;
 	this.name = 'Slack';
 	this.description = 'Slack integration addon';
-  this.listDependencies = ['slack-robot'];
-  var Testcase = mongoose.model('Testcase')
-  var Result = mongoose.model('Result');
+    this.listDependencies = ['slack-robot'];
+    var Testcase = mongoose.model('Testcase')
+    var Result = mongoose.model('Result');
+    
+    var allowAllUsers = true;
+    var allowedUsers = [/jussi/, /wirkus/];
+    
+    var allowAllChannels = false;
+    var allowedChannels = [/jussiva/, /wirkus/, /thread/, /6lowpan/, /clitest/];
 
   if( !nconf.get('slack')) {
     winston.error('slack configuration missing!');
@@ -25,7 +32,7 @@ function AddonSlack (app, server, io, passport){
     }
     
     var allowedChannel = function(channel) {
-        var allowedChannels = [/jussiva/, /wirkus/, /thread/, /6lowpan/];
+        if( allowAllChannels ) return true;
         if( !channel || !channel.name ) return false;
         winston.log('check if channel is valid: '+channel.name);
         return allowedChannels.some( function(ch) {
@@ -35,9 +42,7 @@ function AddonSlack (app, server, io, passport){
         });
     }
     var allowedUser = function(user) {
-        return true; //allow any user to use this for now..
-        
-        var allowedUsers = [/jussi/, /wirkus/];
+        if( allowAllUsers ) return true;
         if( !user || !user.real_name ) return false;
         winston.log('check if user is valid: '+user.real_name);
         return allowedUsers.some( function(usr) {
@@ -54,32 +59,56 @@ function AddonSlack (app, server, io, passport){
     }
     
     var help = function(message, channel, user, m) {
-        channel.send("Test Management Service\nlist testcases\tList all available cases\nlist results\tList 10 latest results\nrun [testcase]\trun individual case");
+        channel.send("```Test Management Service\n:list testcases\tList all available cases\n:list results\tList 10 latest results\n:run [testcase]\t:run individual case\n:help\tThis help```");
     }
     var list = function(message, channel, user, m) {
-         Testcase.query({t: 'distinct', f: 'tcid'}, function(error, list){
-          channel.send(JSON.stringify(list, ' ', '\n'));
+        q = {f: 'tcid', t: 'distinct'};
+        if( m=message.text.match(/^\:list\Wtestcases\W(.*)/)  ) {
+            try {
+                _.extend(q, JSON.parse(m[1]));
+            } catch(e) {
+                console.log(e);
+                channel.send('Invalid format');
+                return;
+            }
+        }
+        Testcase.query(q, function(error, list){
+          channel.send('```'+JSON.stringify(list, ' ', '\n')+'```');
         })
     }
     var results = function(message, channel, user, m) {
-        q = {};
-        if( m=message.text.match(/thread/) ) {
-            if(!q['$or']) q['$or'] = [];
-            q['$or'].push({'exec.sut.cut': 'thread'});
+        q = {l: 10, s: '{"cre.time": -1}', q: {}};
+        if( m=message.text.match(/thread/) || channel.name.match(/thread/) ) {
+            if(!q.q['$or']) q.q['$or'] = [];
+            q.q['$or'].push({'exec.sut.cut': 'thread'});
         }
-        if( m=message.text.match(/6lowpan/) ) {
-            if(!q['$or']) q['$or'] = [];
-            q['$or'].push({'exec.sut.cut': '6LoWPAN'});
+        else if( m=message.text.match(/6lowpan/) || channel.name.match(/6lowpan/)  ) {
+            if(!q.q['$or']) q.q['$or'] = [];
+            q.q['$or'].push({'exec.sut.cut': '6LoWPAN'});
         }
-        console.log('result query: '+JSON.stringify(q));
-        Result.query({l: 10, s: '{"cre.time": -1}', q: JSON.stringify(q)}, function(error, list){
-          var str = 'TC\tResult\n';
-          list.forEach( function (result){
-            if( result.exec && result.exec.verdict ) {
-                str += result.tcid+'\t'+result.exec.verdict+'\n';
+        else if( m=message.text.match(/^\:list\Wresults\W(.*)/)  ) {
+            try {
+                _.extend(q, JSON.parse(m[1]));
+            } catch(e) {
+                console.log(e);
+                channel.send('Invalid format');
+                return;
             }
-          });
-          channel.send(str);
+        }
+        q.q = JSON.stringify(q.q);
+        console.log('result query: '+JSON.stringify(q));
+        Result.query(q, function(error, list){
+            if( error ) {
+                channel.send('Error while query');
+                return;
+            }
+            var str = 'TC\t\tResult\n';
+            list.forEach( function (result){
+                if( result.exec && result.exec.verdict ) {
+                    str += result.tcid+'\t'+result.exec.verdict+'\n';
+                }
+            });
+            channel.send('```'+str+'```');
         })
     }
     var run = function(message, channel, user, m) {
@@ -106,15 +135,25 @@ function AddonSlack (app, server, io, passport){
     }
     
     var handleMessage = function(message) {
-        channel = slack.getChannelGroupOrDMByID(message.channel)
-        user = slack.getUserByID(message.user)
-        if( !allowMessage(channel, user) ) {
-            winston.warn('message not allowed');
-            return;
-        }
-        winston.log('SLACK: '+channel.name+'|'+user.real_name+''+message.text);
-        if( !execute(message, channel, user) ) {
-            channel.send('Unknown command');
+        try {
+            channel = slack.getChannelGroupOrDMByID(message.channel)
+            user = slack.getUserByID(message.user)
+            if( !allowMessage(channel, user) ) {
+                winston.warn('message not allowed');
+                return;
+            }
+            if( !user.hasOwnProperty("real_name") ) {
+                console.log(user);
+                console.log(message.text);
+                return;
+            }
+            winston.log('SLACK: '+channel.name+'|'+user.real_name+''+message.text);
+            if( !execute(message, channel, user) ) {
+                console('Unknown command: '+message.text);
+            }
+        } catch( e ) {
+            console.log(e);
+            console.log(message.text);
         }
     }
 
@@ -125,7 +164,7 @@ function AddonSlack (app, server, io, passport){
       channels = []
       groups = []
       unreads = slack.getUnreadCount()
-
+      winston.info('Slack -connection open');
       // Get all the channels that bot is a member of
       /*channels = {} 
       for( var id, channel in slack.channels ) {
